bfab81e8044f52958b94865ef179aaa4a24f5f7c
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-02-04 19:34:45 +0300

    Complete

--- src/components/ToDo/ListItem/ListItem.js
+++ src/components/ToDo/ListItem/ListItem.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 
 import { FaTrash, FaEdit, FaSave, FaArrowLeft } from "react-icons/fa";
 import styles from "./ListItem.module.scss";
